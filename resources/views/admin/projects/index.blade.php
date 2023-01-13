da361a60c832ee36a969928e37cceceac61e3739
LorenzoTesoro <lorenzotesoro2@gmail.com>
2023-01-13 14:34:19 +0100

    add crud delete

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -22,6 +22,7 @@
         <thead class="table-light">
             <tr>
                 <th>ID</th>
+                <th>Cover Image</th>
                 <th>Title</th>
                 <th>Slug</th>
                 <th>Actions</th>
@@ -31,6 +32,14 @@
             @forelse ($projects as $project)
             <tr class="table-primary">
                 <td scope="row">{{$project->id}}</td>
+                <td>
+                    @if($project->cover_image)
+                    <img width="140" class="img-fluid p-2" src="{{asset('storage/' . $project->cover_image)}}" alt="">
+                    @else
+                    <div class="placeholder p-5 bg-secondary d-flex align-items-center justify-content-center" style="width:140px">Placeholder</div>
+                    @endif
+
+                </td>
                 <td>{{$project->title}}</td>
                 <td>{{$project->slug}}</td>
                 <td>
